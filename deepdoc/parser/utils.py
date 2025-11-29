fd863a32ca2cfe8dd61eb8390b018323c6193f72
Ajay <ajaykmrbel@gmail.com>
2025-11-29 09:45:21 +0530

    some changes in text parser utility to run utf-8

--- deepdoc/parser/utils.py
+++ deepdoc/parser/utils.py
@@ -4,10 +4,12 @@ from rag.nlp import find_codec
 def get_text(fnm: str, binary=None) -> str:
     txt = ""
     if binary:
+        # This block correctly uses find_codec for binary data
         encoding = find_codec(binary)
         txt = binary.decode(encoding, errors="ignore")
     else:
-        with open(fnm, "r") as f:
+        # FIX: Explicitly use UTF-8 encoding to prevent UnicodeDecodeError (e.g., charmap codec error)
+        with open(fnm, "r", encoding="utf-8") as f:
             while True:
                 line = f.readline()
                 if not line:
